5ca8d4e3f4b155aea8a6abb6c0b9485f05de6df9
andysitt <88879646+andysitt@users.noreply.github.com>
2025-09-14 11:29:26 +0800

    Fix Next.js i18n configuration error

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-const withNextIntl = require('next-intl/plugin')('./app/i18n/request.ts');
+const withNextIntl = require('next-intl/plugin')();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
